87a0e6e57e99f4c6cc66cbf7a63aee9f40cb8abd
Yash Vekariya <155796284+YashVekariya0906@users.noreply.github.com>
2026-08-27 07:22:00 +0530

    Time: 0 ms (100%), Space: 8.2 MB (99.37%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        int count = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n - 2; ++i) {
+            // Check if all three characters in the current window are distinct
+            if (s[i] != s[i+1] && s[i] != s[i+2] && s[i+1] != s[i+2]) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
